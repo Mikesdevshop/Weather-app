2d93051987eeb98e1e59e2aa47b9bc8b0a38c2d6
= <Michael.L.Quinn20@gmail.com>
2020-09-30 12:23:05 -1000

    Added new home view.

--- weather/urls.py
+++ weather/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import WeatherHomeView
 
+from .views import index
+app_name = 'weather'
 urlpatterns = [
-    path('', index),
+    path('index/', index, name='weather-index'),
+    path('', WeatherHomeView.as_view(), name='weather-home'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from .models import City
 from .forms import CityForm
 
 import requests
+
+class WeatherHomeView(TemplateView):
+    template_name='weather/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=839ba7e69d5e2b0a1b31dd8a065684d5'
+        cities = City.objects.all()
+
+        weather_data = []
+
+        for city in cities:
+            city_weather = requests.get(url.format(city)).json()
+
+            weather = {
+                'city' : city,
+                'temperature' : city_weather['main']['temp'],
+                'description' : city_weather['weather'][0]['description'],
+                'icon' : city_weather['weather'][0]['icon'],
+            }
+
+            weather_data.append(weather)
+
+        context = {'weather_data' : weather_data}
+
+        return context
+    
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=839ba7e69d5e2b0a1b31dd8a065684d5'
